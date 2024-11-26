727f4bb18bf17b76cdc90e1992c833680bd07b3f
Andrew Vlietstra <vlietstra123@hotmail.com>
2024-11-26 13:58:53 -0500

    Started setting up basic structure of app, copied over styling for project

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+	return (
+		<footer>
+			<small>
+				<p>
+					&copy; Copyright by CorpComment. Intended for learning React,
+					Typescript, Zustand and Context.
+				</p>
+				<p>
+					<span className="u-bold u-italic">Not allowed</span> to use as your
+					own teaching material.
+				</p>
+			</small>
+		</footer>
+	);
+}
