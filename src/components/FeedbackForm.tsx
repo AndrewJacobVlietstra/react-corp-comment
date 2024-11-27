ab975cbce712e747ba6081338e5ff14c2b45a98a
Andrew Vlietstra <vlietstra123@hotmail.com>
2024-11-27 16:55:03 -0500

    Added character counter to show how many characters are left in textarea input, added some onChange logic to control textarea and set a max limit of characters

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,13 +1,35 @@
+import { ChangeEvent, useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
+type TextAreaChangeEvent = ChangeEvent<HTMLTextAreaElement>;
+
 export default function FeedbackForm() {
+	const [text, setText] = useState("");
+	const charCount = MAX_CHARACTERS - text.length;
+
+	const handleTextChange = (e: TextAreaChangeEvent) => {
+		const currentText = e.target.value;
+		if (currentText.length > MAX_CHARACTERS) {
+			return;
+		}
+		return setText(currentText);
+	};
+
 	return (
 		<form className="form">
-			<textarea id="feedback-textarea" placeholder="" spellCheck={false} />
+			<textarea
+				value={text}
+				onChange={handleTextChange}
+				id="feedback-textarea"
+				placeholder=""
+				spellCheck={false}
+			/>
 			<label htmlFor="feedback-textarea">
 				Enter your feedback here, remember to #hashtag the company!
 			</label>
 
 			<div>
-				<p className="u-italic">150</p>
+				<p className="u-italic">{charCount}</p>
 				<button>
 					<span>Submit</span>
 				</button>
